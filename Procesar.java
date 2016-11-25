b79c4b9ea0e89e7ce5d3abb28aaec4a0ffbfdcd2
JOSÉ MARÍA CAMPOS LAHOZ <jmclahoz@gmail.com>
2016-11-25 13:29:48 +0100

    Introducido el número de semana actual.

--- Procesar.java
+++ Procesar.java
@@ -12,6 +12,7 @@
  * colocar en $HOME/bin para empezar a utilizarlo.
  * @version 0.6 Cambiar el formato de la fecha para que se pueda ordenar por
  * orden númerico. Es decir YYYY-MM-DD. Además lo subo a GitHub.
+ * @version 0.7 Añadir varias funcionalidades al programa para mejorarlo.
  */
 
 import java.io.*;
@@ -43,13 +44,18 @@ public class Procesar{
         Scanner sc = new Scanner(System.in);
         String str = " ";
 		  try{
-			FileWriter flS = new FileWriter("/home/o7586t/Documentos/USB_DISK/trabajo/limpio/Mi_TrabajoDiario/registro_diario.otl", true);					  
+			//FileWriter flS = new FileWriter("/home/o7586t/Documentos/USB_DISK/trabajo/limpio/Mi_TrabajoDiario/registro_diario.otl", true);					  
+         // Directorio de pruebas el directorio local.
+			FileWriter flS = new FileWriter("./registro_diario.otl", true);					  
 			// Para que se añada la información en el fichero 'registro_diario.otl' hay que ponerle 'true'.
  
 			BufferedWriter fs = new BufferedWriter(flS);
 
 			Date date = new Date();
 			DateFormat hourdateFormat = new SimpleDateFormat("yyyy-MM-dd - HH:mm:ss ");
+         // Semana en número
+			// DateFormat semanaFormat = new SimpleDateFormat("yyyy-MM-dd - HH:mm:ss ");
+         // 
 			System.out.println();
 			System.out.println("Hora y fecha: " + hourdateFormat.format(date));
 			System.out.println("Introduce el texto. Para terminar un punto único en la línea.");
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Esta aplicación de consola se encargará de la gestión de los procesos diarios de trabajo en la Oficina. Para ello contará por una parte en la entrada de información, y por la otra en la grabación en un fichero de esta información procesada o no en el transcurso del día laboral.
 
-"Salu2"
+Salu2
